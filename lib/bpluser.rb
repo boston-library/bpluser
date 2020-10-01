dcefda73939cf62dc498de523fc73b0b119815ef
Eben English <eben.english@gmail.com>
2020-10-01 16:20:14 -0400

    added user-centric config/initializers and locale translations from commonwealth-vlr-engine

--- lib/bpluser.rb
+++ lib/bpluser.rb
@@ -6,13 +6,18 @@ begin
   require 'active_support/concern' unless defined? ActiveSupport::Concern
   require 'hydra-ldap' unless defined? Hydra::LDAP
 rescue LoadError => e
-  raise "One more more depndencies have not been installed please install the gem referred to in this error #{e.message}"
+  raise "One more more dependencies have not been installed please install the gem referred to in this error #{e.message}"
 end
 require 'bpluser/engine'
+
 module Bpluser
   autoload :Routes, 'bpluser/routes'
 
   def self.add_routes(router, options = {})
     Bpluser::Routes.new(router, options).draw
+  end
+
+  def self.root
+    @root ||= File.expand_path(File.dirname(File.dirname(__FILE__)))
   end
 end
