c313f13c8b86a49c243c49440dd3104345b099f5
Eben English <eben.english@gmail.com>
2020-10-30 16:51:35 -0400

    allow ENV vars in config/*.yml files

--- lib/generators/bpluser/devise_generator.rb
+++ lib/generators/bpluser/devise_generator.rb
@@ -27,13 +27,13 @@ module Bpluser
 
       marker = '# ==> Warden configuration'
       insert_into_file devise_initializer, before: marker do
-        "OMNIAUTH_POLARIS_GLOBAL = YAML.load_file(Rails.root.join('config', 'omniauth-polaris.yml'))[Rails.env]" \
+        "OMNIAUTH_POLARIS_GLOBAL = YAML.safe_load(ERB.new(File.read(Rails.root.join('config', 'omniauth-polaris.yml'))).result, aliases: true)[Rails.env]" \
         "\n  config.omniauth :polaris, title: OMNIAUTH_POLARIS_GLOBAL['title']," \
         "\n                  http_uri: OMNIAUTH_POLARIS_GLOBAL['http_uri']," \
         "\n                  access_key: OMNIAUTH_POLARIS_GLOBAL['access_key']," \
         "\n                  access_id: OMNIAUTH_POLARIS_GLOBAL['access_id']," \
         "\n                  method: OMNIAUTH_POLARIS_GLOBAL['method']\n" \
-        "\n  OMNIAUTH_FACEBOOK_GLOBAL = YAML.load_file(Rails.root.join('config', 'omniauth-facebook.yml'))[Rails.env]" \
+        "\n  OMNIAUTH_FACEBOOK_GLOBAL = YAML.safe_load(ERB.new(File.read(Rails.root.join('config', 'omniauth-facebook.yml'))).result, aliases: true)[Rails.env]" \
         "\n  config.omniauth :facebook, OMNIAUTH_FACEBOOK_GLOBAL['facebook_key']," \
         "\n                  OMNIAUTH_FACEBOOK_GLOBAL['facebook_secret']," \
         "\n                  scope: OMNIAUTH_FACEBOOK_GLOBAL['facebook_scope']\n\n  "
